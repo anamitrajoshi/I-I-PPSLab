db7f849248513ae1deac92e11b119c5ad8e5da00
anamitrajoshi <120377524+anamitrajoshi@users.noreply.github.com>
2023-01-29 18:36:18 +0530

    Create palindrome.c

---
+++ cycle 1 expression evaluation d/palindrome.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int num,rev,sum,r,n;
+sum=0;
+rev=0;
+
+printf("enter a number");
+scanf("%d",&num);
+n = num;
+while(n>0){
+	r=n%10;
+	sum=sum+r;
+	rev=(rev*10)+ r;
+	n=n/10;
+	}
+	
+printf("\nthe sum of the digits is %d",sum);
+
+if(num==rev)
+	printf("\nit is a palindrome");
+else
+	printf("\nit is not a palindrome");
+
+return;
+}
