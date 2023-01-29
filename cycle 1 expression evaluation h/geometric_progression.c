4657f8437f2c0bddfb802e5f7cb406ac6668488f
anamitrajoshi <120377524+anamitrajoshi@users.noreply.github.com>
2023-01-29 19:19:46 +0530

    Create geometric_progression.c

---
+++ cycle 1 expression evaluation h/geometric_progression.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+  int x,i,n;
+  float sum=1.0;
+  printf("enter the value of n");
+  scanf("%d",&n);
+  printf("enter the value of x");
+  scanf("%d",&x);
+  for(i=1;i<=n;i++0{
+    sum=sum+pow(x,i);
+  }
+  printf("sum of the geometric progression is %f",sum);
+  return 0;
+      }
